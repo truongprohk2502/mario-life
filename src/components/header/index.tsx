00320e050a0ef98ee24159abcc3ba7b258322968
Nguyen Dinh Truong <nguyendinhtruong98@gmail.com>
2023-05-21 17:52:21 +0700

    Add some states

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,43 +1,27 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
-import { QwikLogo } from "../icons/qwik";
+import { component$, useStyles$ } from "@builder.io/qwik";
 import styles from "./styles.css?inline";
+import { Link } from "@builder.io/qwik-city";
 
 export default component$(() => {
-  useStylesScoped$(styles);
+  useStyles$(styles);
 
   return (
     <header>
-      <div class="logo">
-        <a href="https://qwik.builder.io/" target="_blank" title="qwik">
-          <QwikLogo />
-        </a>
-      </div>
-      <ul>
-        <li>
-          <a
-            href="https://qwik.builder.io/docs/components/overview/"
-            target="_blank"
-          >
-            Docs
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://qwik.builder.io/examples/introduction/hello-world/"
-            target="_blank"
-          >
-            Examples
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://qwik.builder.io/tutorial/welcome/overview/"
-            target="_blank"
-          >
-            Tutorials
-          </a>
-        </li>
-      </ul>
+      <nav>
+        <img src="/mario_block.jpeg" alt="logo" width={40} height={50} />
+        <h1>Mario Life</h1>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 });
