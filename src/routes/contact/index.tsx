fc7537c77d6d70e9043562917561a0a89dee97c8
Nguyen Dinh Truong <nguyendinhtruong98@gmail.com>
2023-05-21 19:43:27 +0700

    Add contact me form

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -1,8 +1,21 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import {
+  component$,
+  useSignal,
+  useStore,
+  useStylesScoped$,
+} from "@builder.io/qwik";
 import styles from "./styles.css?inline";
+
+interface IFormData {
+  name: string;
+  message: string;
+}
 
 export default component$(() => {
   useStylesScoped$(styles);
+
+  const formVisible = useSignal<boolean>(false);
+  const formState = useStore<IFormData>({ name: "", message: "" });
 
   return (
     <article>
@@ -13,6 +26,43 @@ export default component$(() => {
         nesciunt! Nam, ratione tempore? Quod, error fuga maxime consectetur
         dolorum consequuntur labore numquam.
       </p>
+
+      <button onClick$={() => (formVisible.value = true)}>Contact Me</button>
+
+      {formVisible.value && (
+        <form
+          preventdefault:submit
+          onSubmit$={() => {
+            console.log(formState.name, formState.message);
+            formState.name = "";
+            formState.message = "";
+          }}
+        >
+          <label>
+            <span>Your name:</span>
+            <input
+              type="text"
+              value={formState.name}
+              onInput$={(e) =>
+                (formState.name = (e.target as HTMLInputElement).value)
+              }
+            />
+          </label>
+          <label>
+            <span>Your message:</span>
+            <textarea
+              value={formState.message}
+              onInput$={(e) =>
+                (formState.message = (e.target as HTMLInputElement).value)
+              }
+            ></textarea>
+          </label>
+          <button>Send</button>
+
+          <p>{formState.name}</p>
+          <p>{formState.message}</p>
+        </form>
+      )}
     </article>
   );
 });
